16d9b8beb01b5e57694570c808caa396adb64953
Olzhas Ibrayev <95501759+ibraevsoft@users.noreply.github.com>
2022-11-01 21:38:22 +0600

    video-feedback

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,12 +2,28 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-player.on('timeupdate', function () {
-  console.log('played the video!');
-});
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 2000)
+);
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
+const inputEl = document.querySelector('input');
+const textAreaEl = document.querySelector('textarea');
+const formEl = document.querySelector('form');
 
+formEl.addEventListener(
+  'input',
+  throttle(() => {
+    inputFoo();
+  }, 500)
+);
+
+function inputFoo() {
+  const data = {
+    email: `${inputEl.value}`,
+    message: `${textAreaEl.value}`,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+}
+
+const parsedData = localStorage.getItem('feedback-form-state');
+const dataValues = JSON.parse(parsedData);
+
+if (dataValues !== null) {
+  inputEl.value = dataValues.email;
+  textAreaEl.textContent = dataValues.message;
+}
+
+formEl.addEventListener('submit', submitFoo);
+
+function submitFoo(event) {
+  event.preventDefault();
+  let currentData = {
+    email: inputEl.value,
+    message: textAreaEl.value,
+  };
+  console.log(currentData);
+  localStorage.removeItem('feedback-form-state');
+
+  event.target.reset();
+  textAreaEl.textContent = '';
+}
